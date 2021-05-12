2ab2710d338425ffdd038ebc606d5fc5a2373fd7
Muhammad Ihsan Abdul Hakim <75382503+ihsanahakiim@users.noreply.github.com>
2021-05-12 12:12:08 +0700

    Update map.h

--- map.h
+++ map.h
@@ -1,5 +1,5 @@
-#ifndef MAP_H
-#define MAP_H
+#ifndef MAP.H
+#define MAP.H
 
 void MapMaker(int n){
     for (int i = 0; i<n; i++){
@@ -15,10 +15,124 @@ void MapMaker(int n){
     }
 }
 
-void LeftMap (int n){
+char FirstLetter(string a){
+    char* array;
+    string nama(a);
+    array = &nama[0];
+    return array[0];
+}
+void MapConverter(int n, string a, string b, int x, int y, int u, int v, int q, int w, int e, int r){
+    char P = FirstLetter(a);
+    char T = FirstLetter(b);
+
+    for (int i = 0; i<n; i++){
+        for (int j = 0; j<n; j++){
+            if(x == u && y == v || x == q  & y == w || x == e && y == r){
+                cout << "Obyek menabrak";
+                break;
+            }
+            else if(u == q && v == w || u == e && v == r || q == e && w == r){ 
+                cout << "Obyek menabrak";
+                break;
+            }
+            else{
+                if (i == 0 ||i == n-1 || j == 0 || j == n-1){
+                    cout << "x";
+                }
+                else{
+                    if (i == x && j == y){
+                        cout << P;
+                    }
+                    else if (i == u && j == v){
+                        cout << T;
+                    }
+                    else if (i == q && j == w || i == e && j == r){
+                        cout << "L";
+                    }
+                    else{
+                        cout << "-";
+                    }
+                }    
+            }
+        }
+        cout << endl;
+    }
+}
+
+void LeftMap (int n, string a, string b, int x, int y, int u, int v, int q, int w, int e, int r){
+    char P = FirstLetter(a);
+    char T = FirstLetter(b);
     for (int i = 0; i<n; i++){
         for (int j = n ; j>0; j--){
-            if (i == n-1 || i == 0 || j == n || j == 1){
+            if(x == u && y == v || x == q  & y == w || x == e && y == r){
+                cout << "Obyek menabrak";
+                break;
+            }
+            else if(u == q && v == w || u == e && v == r || q == e && w == r){ 
+                cout << "Obyek menabrak";
+                break;
+            }
+            else{  
+                if (i == n-1 || i == 0 || j == n || j == 1){
+                cout << "x"; 
+                }    
+                else{
+                    if (i == n-(y+1) && j == n-x){
+                        cout << P;
+                    }
+                    else if (i == n-(v+1) && j == n-u){
+                        cout << T;
+                    }
+                    else if (i == n-(w+1) && j == n-q || i == n-(r+1) && j == n-e){
+                        cout << "L";
+                    }
+                    else{
+                        cout << "-";
+                    }   
+                }  
+            }      
+        }
+    cout << endl;
+    }
+}          
+void RightMap (int n, string a, string b, int x, int y, int u, int v, int q, int w, int e, int r){
+    char P = FirstLetter(a);
+    char T = FirstLetter(b);
+    for (int i = 0; i<n; i++){
+        for (int j = n ; j>0; j--){
+            if(x == u && y == v || x == q  & y == w || x == e && y == r){
+                cout << "Obyek menabrak";
+                break;
+            }
+            else if(u == q && v == w || u == e && v == r || q == e && w == r){ 
+                cout << "Obyek menabrak";
+                break;
+            }
+            else{
+                if (i == n-1 || i == 0 || j == n || j == 1){
+                cout << "x"; 
+                }   
+                else{
+                    if (i == y && j == x+1){
+                        cout << P;
+                    }
+                    else if (i == v && j == u+1){
+                        cout << T;
+                    }
+                    else if (i == w && j == q+1 || i == r && j == e+1){
+                        cout << "L";
+                    }
+                    else{
+                        cout << "-";
+                    }   
+                } 
+            }   
+        }
+    cout << endl;
+    }void MapMaker(int n){
+    for (int i = 0; i<n; i++){
+        for (int j = 0; j<n; j++){
+            if (i == 0 ||i == n-1 || j == 0 || j == n-1){
                 cout << "x";
             }
             else{
@@ -29,16 +143,120 @@ void LeftMap (int n){
     }
 }
 
-void  RightMap (int n){
-    for (int i = n; i>0; i--){
+char FirstLetter(string a){
+    char* array;
+    string nama(a);
+    array = &nama[0];
+    return array[0];
+}
+void MapConverter(int n, string a, string b, int x, int y, int u, int v, int q, int w, int e, int r){
+    char P = FirstLetter(a);
+    char T = FirstLetter(b);
+
+    for (int i = 0; i<n; i++){
         for (int j = 0; j<n; j++){
-            if (i == 1 ||i == n || j == 0 || j == n-1){
-                cout << "x";
+            if(x == u && y == v || x == q  & y == w || x == e && y == r){
+                cout << "Obyek menabrak";
+                break;
+            }
+            else if(u == q && v == w || u == e && v == r || q == e && w == r){ 
+                cout << "Obyek menabrak";
+                break;
             }
             else{
-                cout << "-";
+                if (i == 0 ||i == n-1 || j == 0 || j == n-1){
+                    cout << "x";
+                }
+                else{
+                    if (i == x && j == y){
+                        cout << P;
+                    }
+                    else if (i == u && j == v){
+                        cout << T;
+                    }
+                    else if (i == q && j == w || i == e && j == r){
+                        cout << "L";
+                    }
+                    else{
+                        cout << "-";
+                    }
+                }    
             }
         }
         cout << endl;
     }
 }
+
+void LeftMap (int n, string a, string b, int x, int y, int u, int v, int q, int w, int e, int r){
+    char P = FirstLetter(a);
+    char T = FirstLetter(b);
+    for (int i = 0; i<n; i++){
+        for (int j = n ; j>0; j--){
+            if(x == u && y == v || x == q  & y == w || x == e && y == r){
+                cout << "Obyek menabrak";
+                break;
+            }
+            else if(u == q && v == w || u == e && v == r || q == e && w == r){ 
+                cout << "Obyek menabrak";
+                break;
+            }
+            else{  
+                if (i == n-1 || i == 0 || j == n || j == 1){
+                cout << "x"; 
+                }    
+                else{
+                    if (i == n-(y+1) && j == n-x){
+                        cout << P;
+                    }
+                    else if (i == n-(v+1) && j == n-u){
+                        cout << T;
+                    }
+                    else if (i == n-(w+1) && j == n-q || i == n-(r+1) && j == n-e){
+                        cout << "L";
+                    }
+                    else{
+                        cout << "-";
+                    }   
+                }  
+            }      
+        }
+    cout << endl;
+    }
+}          
+void RightMap (int n, string a, string b, int x, int y, int u, int v, int q, int w, int e, int r){
+    char P = FirstLetter(a);
+    char T = FirstLetter(b);
+    for (int i = 0; i<n; i++){
+        for (int j = n ; j>0; j--){
+            if(x == u && y == v || x == q  & y == w || x == e && y == r){
+                cout << "Obyek menabrak";
+                break;
+            }
+            else if(u == q && v == w || u == e && v == r || q == e && w == r){ 
+                cout << "Obyek menabrak";
+                break;
+            }
+            else{
+                if (i == n-1 || i == 0 || j == n || j == 1){
+                cout << "x"; 
+                }   
+                else{
+                    if (i == y && j == x+1){
+                        cout << P;
+                    }
+                    else if (i == v && j == u+1){
+                        cout << T;
+                    }
+                    else if (i == w && j == q+1 || i == r && j == e+1){
+                        cout << "L";
+                    }
+                    else{
+                        cout << "-";
+                    }   
+                } 
+            }   
+        }
+    cout << endl;
+    }
+}   
+#endif
