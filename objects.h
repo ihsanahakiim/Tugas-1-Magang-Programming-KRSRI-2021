e1e53831c1c766410684100a0dc50b770e9f8074
Muhammad Ihsan Abdul Hakim <75382503+ihsanahakiim@users.noreply.github.com>
2021-05-12 13:45:39 +0700

    Update objects.h

--- objects.h
+++ objects.h
@@ -1,17 +1,18 @@
+//objects.h
 #ifndef OBJECT.H
 #define OBJECT.H
 
-typedef enum{
+typedef enum{   // mendeklarasikan variabel enumeration status penyalaan lilin
     off, on
 } status;
 
-void CandleEnumeration1(int q, int w, int n){
-    int* nulnumber;
+void CandleEnumeration1(int q, int w, int n){ // deklarasi fungsi enumerasi lilin 1
+    int* nulnumber;     // pemanggil pointer variabel NULL
     nulnumber = NULL;
     status light1, light2;
-    light1 = off;
-    light2 = on;
-    if(q == n-1  || w == 0 || &q == nulnumber || &w == nulnumber){
+    light1 = off;   // status nyala lilin bernilai 0 (mati)
+    light2 = on;    // status nyala lilin bernilai 1 (nyala)
+    if(q == n-1  || w == 0 || &q == nulnumber || &w == nulnumber){ // syarat kondisi enumerasi
         cout << "Status lilin 1: " << light1 << endl;
     }
     else{
@@ -19,14 +20,14 @@ void CandleEnumeration1(int q, int w, int n){
     }
 }
 
-void CandleEnumeration2(int e, int r, int n){
-    int* nulnumber;
+void CandleEnumeration2(int e, int r, int n){ // deklarasi fungsi enumerasi lilin 1
+    int* nulnumber;     // pemanggil pointer variabel NULL
     nulnumber = NULL;
     status light1, light2;
-    light1 = off;
-    light2 = on;
-    if(e == n-1  || r == 0 || &e == nulnumber || &r == nulnumber){
-        cout << "Status lilin 2: " << light1 << endl;
+    light1 = off;   // status nyala lilin bernilai 0 (mati)
+    light2 = on;    // status nyala lilin bernilai 1 (nyala)
+    if(e == n-1  || r == 0 || &e == nulnumber || &r == nulnumber){ //syarat kondisi enumerasi
+        cout << "Status lilin 2: " << light1 << endl; 
     }
     else{
         cout << "Status lilin 2: " << light2 << endl;
